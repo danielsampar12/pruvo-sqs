306dcae7e5755eaa5a52c247887c70ac7a67884a
Daniel Sampar <daniel.sampar12@gmail.com>
2023-03-27 02:24:17 -0300

    feat: serve statics

--- rest-api/index.ts
+++ rest-api/index.ts
@@ -1,11 +1,15 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import sqsRouter from "./src/routers/sqsRouter";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const dir = process.env.PWD;
+
+app.use(express.static(path.join(dir || __dirname, "public")));
 
 app.use(express.json());
 app.use("/sqs", sqsRouter);
